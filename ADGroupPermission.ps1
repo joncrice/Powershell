02eadca7cb5c70245748a8f77d17990e063bca9f
joncrice <joncrice@gmail.com>
2016-08-10 17:00:16 -0400

    Recurse through AD groups for permission
    Retrieve the access level to each AD group for the AD group specified.

--- ADGroupPermission.ps1
+++ ADGroupPermission.ps1
@@ -1,3 +1,4 @@
+import-module activedirectory  
 CD AD:
 
 # This array will hold the report output.
@@ -7,6 +8,8 @@ $ADGroups = Get-ADObject -SearchBase (Get-ADDomain).DistinguishedName -LDAPFilte
 
 ForEach ($ADGroup in $ADGroups) {
 
+$GroupName = get-acl -Path "AD:$ADGroup" | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "GROUPNAME" -and $_.ObjectType -notlike "00000000-0000-0000-0000-000000000000" } | Select-object -ExpandProperty IdentityReference
+$Permission = get-acl -Path "AD:$ADGroup" | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "GROUPNAME" -and $_.ObjectType -notlike "00000000-0000-0000-0000-000000000000" } | Select-object -ExpandProperty ActiveDirectoryRights 
 
 
 $output = New-Object psobject
