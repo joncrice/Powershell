500f329d483dc4908e06c6396ddadef0f7daa9c5
joncrice <joncrice@gmail.com>
2016-08-10 14:57:52 -0400

    Made generic

--- get-msoluserlicence_V6.ps1
+++ get-msoluserlicence_V6.ps1
@@ -4,15 +4,15 @@ $filedate = $filedate -replace '\s.+$'
 $filename += $filedate
 $filename += ".txt"
 
-$pass = get-content C:\users\jrice05\cred.txt | convertto-securestring
-$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "jrice05@tufts.edu",$pass
+$pass = get-content C:\users\XXXXX\cred.txt | convertto-securestring
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "XXXXX@domain.com",$pass
 
 Start-Transcript -Path "c:\temp\$filename"
 
 Connect-MsolService -Credential $credentials
 #Get-MsolUser -All | where {$_.isLicensed -eq $true -and $_.Licenses[0].ServiceStatus[7].ProvisioningStatus -eq "Success"}
-#Get-MsolUser -UserPrincipalName jrice05@tufts.edu | where {$_.isLicensed -eq $true -and $_.Licenses[0].ServiceStatus[7].ProvisioningStatus -eq "Disabled"}
-#Get-MsolUser -UserPrincipalName jrice05@tufts.edu | Select-Object -ExpandProperty Licenses | Select-Object -ExpandProperty ServiceStatus
+#Get-MsolUser -UserPrincipalName XXXXX@domain.com | where {$_.isLicensed -eq $true -and $_.Licenses[0].ServiceStatus[7].ProvisioningStatus -eq "Disabled"}
+#Get-MsolUser -UserPrincipalName XXXXX@domain.com | Select-Object -ExpandProperty Licenses | Select-Object -ExpandProperty ServiceStatus
 
 $users = Get-MsolUser -all | where {$_.isLicensed -eq "True"}
 foreach ($user in $users) {
@@ -41,7 +41,7 @@ Compare-Object (Get-Content c:\temp\today.txt) (Get-Content C:\temp\yesterday.tx
 
 #Send email message of diffs
 $body = Get-Content -Path $filename1 -Raw
-Send-MailMessage -To "Jonathan Rice <jrice05@tufts.edu>" -From "Jonathan Rice <jrice05@tufts.edu>" -Subject "Office 365 Exchange licensing change report" -Body $body -SmtpServer smtp.tufts.edu
+Send-MailMessage -To "Firstname Lastname <XXXXX@domain.com>" -From "Firstname Lastname <XXXXX@domain.com>" -Subject "Office 365 Exchange licensing change report" -Body $body -SmtpServer smtp.tufts.edu
 
 Stop-Transcript
 
