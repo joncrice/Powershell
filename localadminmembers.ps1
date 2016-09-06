fc54f6ea726360e1fb1c032f676a751d0e771819
joncrice <joncrice@gmail.com>
2016-09-06 15:01:42 -0400

    Made generic

--- localadminmembers.ps1
+++ localadminmembers.ps1
@@ -1,21 +1,15 @@
-
+﻿$Computers = @("COMPUTER1","COMPUTER2")
 
 ForEach ($computername in $computers) {
     $ADMINS = ""
-    Write-Host "Local Admins for $computername"
+    Write-Host "====== Local Admins for $computername ======"
     $computername = $computername.toupper()
     $ADMINS = get-wmiobject -computername $computername -query "select * from win32_groupuser where GroupComponent=""Win32_Group.Domain='$computername',Name='administrators'""" | % {$_.partcomponent}
-    #write-host $ADMINS
+
     foreach ($ADMIN in $ADMINS) {
                 $admin = $admin -replace ".*Name=" 
                 $admin = $admin -replace '"', ""
                 write-host $ADMIN
-                $objOutput = New-Object PSObject -Property @{
-                    UserName = $admin.split("")[1]
-                }#end object
-
-    $objreport+=@($objoutput)
-    #Write-Host $objreport
-    }#end for
-    }
+    }#end for $admin
+    }#end for $computername
 
